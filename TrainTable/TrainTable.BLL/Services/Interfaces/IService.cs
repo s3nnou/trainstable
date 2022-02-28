3a5cd1611c254b167d95984a3d8552f9d5923564
1-Accelerator-1 <max.pashuta@mail.ru>
2022-03-01 00:12:01 +0300

    Created Train service to BLL.

---
+++ TrainTable/TrainTable.BLL/Services/Interfaces/IService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TrainTable.BLL.Services.Interfaces
+{
+    /// <summary>
+    /// This interface sets a set of methods that the proxy should implement.
+    /// </summary>
+    /// <typeparam name="T">The type of model that is the object representation of the table.</typeparam>
+    public interface IService<T>
+    {
+        /// <summary>
+        /// The method validates and creates an object in the database using the repository.
+        /// </summary>
+        /// <param name="entity">The object to insert into the database.</param>
+        Task<T> Create(T entity);
+
+        /// <summary>
+        /// The method reads an object from in the database using the repository.
+        /// </summary>
+        /// <param name="id">The object id to read from the database.</param>
+        /// <returns>The read object from the database.</returns>
+        Task<T> ReadById(int id);
+
+        /// <summary>
+        /// The method validates and updates an object in the database using the repository.
+        /// </summary>
+        /// <param name="entity">The object to update in the database.</param>
+        Task<T> Update(T entity);
+
+        /// <summary>
+        /// The method validates and deletes an object in the database using the repository.
+        /// </summary>
+        /// <param name="id">The object id to remove from the database.</param>
+        Task Delete(int id);
+
+        /// <summary>
+        /// The method reads all objects from in the database using the repository.
+        /// </summary>
+        /// <returns>The list of objects read from the database.</returns>
+        IReadOnlyCollection<T> ReadAll();
+
+        IReadOnlyCollection<T> ReadAll(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ TrainTable/TrainTable.BLL/Services/TrainService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TrainTable.BLL.Services.Interfaces;
+using TrainTable.DAL;
+using TrainTable.DAL.Models;
+
+namespace TrainTable.BLL.Services
+{
+    internal class TrainService : IService<Train>
+    {
+        private readonly IRepository<Train> _repository;
+
+        public TrainService(IRepository<Train> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Train> Create(Train entity)
+        {
+            ValidateTrain(entity);
+
+            Train createdTrain = await _repository.Create(entity);
+            return createdTrain;
+        }
+
+        public async Task Delete(int id)
+        {
+            await _repository.Delete(id);
+        }
+
+        public IReadOnlyCollection<Train> ReadAll()
+        {
+            var trains = _repository.ReadAll().ToList();
+            return trains;
+        }
+
+        public IReadOnlyCollection<Train> ReadAll(Expression<Func<Train, bool>> predicate)
+        {
+            var trains = _repository.ReadAll().Where(predicate).ToList();
+            return trains;
+        }
+
+        public async Task<Train> ReadById(int id)
+        {
+            var train = await _repository.ReadById(id);
+            return train;
+        }
+
+        public async Task<Train> Update(Train entity)
+        {
+            ValidateTrain(entity);
+
+            var train = await _repository.Update(entity);
+            return train;
+        }
+
+        private void ValidateTrain(Train train)
+        {
+            if (train == null)
+            {
+                throw new ArgumentNullException("train", "Value cannot be null.")
+            }
+
+            if (string.IsNullOrEmpty(train.Name))
+            {
+                throw new Exception("Train name cannot be empty.");
+            }
+
+            if (train.Departure == default)
+            {
+                throw new Exception("Train name cannot be empty.");
+            }
+
+            if (train.Destination == default)
+            {
+                throw new Exception("Train name cannot be empty.");
+            }
+
+            if (IsStartDateGreaterEndDate(train))
+            {
+                throw new Exception("The start date of the event must not be greater than the end date of the event.");
+            }
+
+            if (IsStartOrEndDateLessDateNow(train))
+            {
+                throw new Exception("The start or end date of the event must not be less than today's date.");
+            }
+        }
+
+        private bool IsStartDateGreaterEndDate(Train train)
+        {
+            var isStartDateGreaterEndDate = train.DepertureTime > train.DestinationTime;
+            return isStartDateGreaterEndDate;
+        }
+
+        private bool IsStartOrEndDateLessDateNow(Train train)
+        {
+            var isStartOrEndDateLessDateNow = (train.DepertureTime < DateTimeOffset.Now) || (train.DestinationTime < DateTimeOffset.Now);
+            return isStartOrEndDateLessDateNow;
+        }
+    }
+}
