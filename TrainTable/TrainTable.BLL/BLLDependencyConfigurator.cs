5eec52895a23706099b3721bff1fce3c8e4b43ca
1-Accelerator-1 <max.pashuta@mail.ru>
2022-03-01 19:42:52 +0300

    Added bll services to di container.

---
+++ TrainTable/TrainTable.BLL/BLLDependencyConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrainTable.BLL.Services;
+using TrainTable.BLL.Services.Interfaces;
+using TrainTable.DAL;
+using TrainTable.DAL.Models;
+
+namespace TrainTable.BLL
+{
+    public static class BLLDependencyConfigurator
+    {
+        public static void BLLDependencyConfig(this IServiceCollection services, string connectionString)
+        {
+            services.DALDependencyConfig(connectionString);
+
+            services.AddScoped<IService<Train>, TrainService>();
+            services.AddScoped<IService<City>, CityService>();
+            services.AddScoped<IService<FavoriteTrain>, FavoriteTrainService>();
+        }
+    }
+}
